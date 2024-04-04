622aab0efcec3924e0fb8470ff1b11ced905bd68
stacks-loops <p.cronin.connolly@gmail.com>
2024-04-04 14:48:17 -0400

    localhost was not proxing, had to change proxy to http://127.0.0.1:5000

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,20 @@ function App() {
   const [data, setData] = useState([{}])
 
   useEffect(() => {
-    fetch("http://localhost/users").then(
-    res => res.json()
-    ).then(
-      data => {
+    fetch("/users").then(
+    res => {
+      if (!res.ok) {
+        throw new Error("Network response was in fact not okay")
+      }
+      return res.json()
+    })
+    .then(data => {
         setData(data)
         console.log(data)
-      }
-    )
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error)
+      })
   }, [])
 
   return (
