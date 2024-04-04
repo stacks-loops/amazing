de2ae238bde502d3b56afb069e6723f9bce82089
stacks-loops <p.cronin.connolly@gmail.com>
2024-04-04 18:14:28 -0400

    user class

--- flask-server/models.py
+++ flask-server/models.py
@@ -4,5 +4,10 @@ from config import db
 user_patient_association = db.Table(
     'user_patient_association',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('patient_id' db.Integer, db.ForeignKey('patient.id'))
+    db.Column('patient_id', db.Integer, db.ForeignKey('patient.id'))
 )
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    # add the rest of attributres and auth stuff here
