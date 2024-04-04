2d62efb88cb637e9bb447a113a5080f240573382
stacks-loops <p.cronin.connolly@gmail.com>
2024-04-04 18:12:42 -0400

    user patient association

--- flask-server/models.py
+++ flask-server/models.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
-from server import db
+from config import db
+
+user_patient_association = db.Table(
+    'user_patient_association',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('patient_id' db.Integer, db.ForeignKey('patient.id'))
+)
