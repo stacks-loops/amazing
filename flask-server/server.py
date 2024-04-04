14facb08e8642935b88b19c2398affa22aff9334
stacks-loops <p.cronin.connolly@gmail.com>
2024-04-04 15:49:55 -0400

    batman deleted

--- flask-server/server.py
+++ flask-server/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/users')
 def users():
-    return {"users": ["Ragnarok", "Violet", "parker", "jason", "toledo", "batman"]}
+    return {"users": ["Ragnarok", "Violet", "parker", "jason", "toledo"]}
 
 if __name__ == "__main__":
     app.run(debug=True)
