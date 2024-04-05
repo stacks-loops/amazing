814b86b0eb60ded887ae5546b85459fec5557fe4
stacks-loops <p.cronin.connolly@gmail.com>
2024-04-05 06:51:55 -0400

    changed entry point

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,1 +1,13 @@
-#!/usr/bin/env python3
+from flask import Flask, jsonify
+from config import app, db
+from models import User, Patient
+
+#users API route
+
+@app.route('/users')
+def users():
+    users = User.query.all()
+    return jsonify({'users': [user.username for user in users]})
+
+if __name__ == "__main__":
+    app.run(debug=True)
