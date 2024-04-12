bb9f1c3d25b306e206af7470c24b2fde2f5bdde7
stacks-loops <p.cronin.connolly@gmail.com>
2024-04-12 19:31:51 -0400

    header

---
+++ client/src/components/Header.js
@@ -0,0 +1,28 @@
+import { Navlink } from "react-router-dom";
+
+function Header({ logoutUser }) {
+    
+    function handleLogout() {
+        fetch('/logout', {
+            method: 'DELETE'
+        }).then( resp => {
+            if (resp.ok) {
+                logoutUser()
+            }
+        })
+    }
+
+    return (
+        <header>
+            <h1>
+                <span>Put something here</span>
+                Patients
+            </h1>
+            <nav>
+                <NavLink to="/PatientList">Your Patients</NavLink>
+            </nav>
+            <button onClick={handleLogout}>Logout</button>
+        </header>
+    )
+
+}
