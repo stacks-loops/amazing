fc4a3d103abf5e5c76e098167a07fad72f6adb37
stacks-loops <p.cronin.connolly@gmail.com>
2024-04-14 12:40:59 -0400

    formik signup working

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
 import httpClient from "../httpClient";
+import { Formik, Form, Field, ErrorMessage } from "formik"
+import * as Yup from "yup"
 
 function SignupPage() {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [error, setError] = useState('')
+  const initialValues = {
+    email: "",
+    password: "",
+  }
 
-  const createUser = async () => {
+  const handleSubmit = async (values : any , { setSubmitting, setFieldError }) => {
+
     try {
       const resp = await httpClient.post("//localhost:5000/signup", {
-        email,
-        password,
+        email: values.email,
+        password: values.password,
       });
 
       // good login takes you to this page
@@ -18,43 +21,53 @@ function SignupPage() {
         window.location.href = "/";
       } else {
         console.error("Unexpected response:", resp)
-        setError("Unexpected error")
+        setFieldError("Unexpected error")
       }
 
       // console.log(resp.data)
       // bad login error handling
     } catch (error) {
       console.error("error creating user", error)
-      setError("an error occured")
+      setFieldError("an error occured")
+    } finally {
+      setSubmitting(false)
     }
   };
+  const validationSchema = Yup.object({
+    email: Yup.string().email("Invalid email address").required("Valid email required"),
+    password: Yup.string().required("Password required")
+
+  })
   // Forms for login
   return (
     <div>
       <h1>Join the Spalla Network</h1>
-      <form>
-        <div>
-          <label>Email</label>
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            id=""
-          />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            id=""
-          />
-        </div>
-        <button type="button" onClick={() => createUser()}>
-          Submit
-        </button>
-      </form>
+      <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+      >
+        {(formik) => (
+          <Form>
+            <div>
+              <label htmlFor="email">Email</label>
+              <Field type="text" id="email" name="email" />
+              <ErrorMessage name="email" component="div" />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <Field type="password" id="password" name="password" />
+              <ErrorMessage name="password" component="div" />
+            </div>
+            <div>
+              <button type="submit" disabled={formik.isSubmitting}>
+                {formik.isSubmitting ? "Submitting..." : "Submit"}
+              </button>
+              <ErrorMessage name="submit" component ="div" />
+            </div>
+          </Form>
+        )}
+        </Formik>
     </div>
   );
 }
